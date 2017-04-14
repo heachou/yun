eb5b602f42f9e1a338b3255a65bc435cab4a22c2
hea <1544500164@qq.com>
2017-04-14 12:45:59 +0800

    add components
    Signed-off-by: hea <1544500164@qq.com>

---
+++ src/components/checkBox.vue
@@ -0,0 +1,49 @@
+<template>
+	<div class="checkBox" v-if="checkedNames != 0">
+		<header>
+			<button @click="cancelSelected">取消</button>
+			<h2>已选定{{checkedNames.length}}项</h2>
+			<button @click="selectAll">全选</button>
+		</header>
+	</div>
+</template>
+<script type="text/javascript">
+	export default{
+		props:['checkedNames','cancelSelected','selectAll']
+	}
+</script>
+<style type="text/css" lang="less">
+	.checkBox{
+		position: fixed;
+		left:0;
+		top:0;
+		height: 60px;
+		width: 100%;
+		background: rgba(0,0,0,.7);
+		z-index: 10;
+		header{
+			display: flex;
+			width: 100%;
+			align-items: center;
+			height: 100%;
+			padding: 0 15px;
+			color: #fff;
+			justify-content: space-between;
+			box-sizing: border-box;
+			 button{
+				color: #fff;
+				background: transparent;
+				padding: 6px 12px;
+				border-radius: 2px;
+				font-size:12px; 
+				outline: none;
+				border:1px solid #d2d2d2;
+			}
+			h2{
+				font-size: 14px;
+				letter-spacing: 1px;
+				font-weight: normal;
+			}
+		}
+	}
+</style>
---
+++ src/components/tabUpload.vue
@@ -0,0 +1,151 @@
+<template>
+	<div class="categoryBox" v-show="selected1 == 'upload'">
+		<div class="uploadArea">
+			<div class="uploadcontent">
+				<p class="text-center">选择上传文件类型</p>
+				<ul>
+					<li>
+						<div>
+							<img src="../assets/images/dog.jpg">
+						</div>
+						<p>图片</p>
+					</li>
+					<li>
+						<div>
+							<img src="../assets/images/dog.jpg">
+						</div>
+						<p>文档</p>
+					</li>
+					<li>
+						<div>
+							<img src="../assets/images/dog.jpg">
+						</div>
+						<p>视屏</p>
+					</li>
+					<li>
+						<div>
+							<img src="../assets/images/dog.jpg">
+						</div>
+						<p>音乐</p>
+					</li>
+					<li>
+						<div>
+							<img src="../assets/images/dog.jpg">
+						</div>
+						<p>全部</p>
+					</li>
+				</ul>
+			</div>
+		</div>
+		<div class="mask" @click="closeModal"></div>
+	</div>
+</template>
+<script type="text/javascript">
+	export default{
+		props:['selected1','closeModal'],
+		data(){
+			return{
+
+			}
+		}
+	}
+</script>
+<style lang="less">
+	@keyframes myMove{
+		0%{
+			transform:translateY(-200px);
+		}
+		100%{
+			transform:translateY(0px);
+		}
+	}
+	@keyframes imgscale{
+		0%{
+			transform:scale(0.3);
+		}
+		80%{
+			transform:scale(1.1);
+		}
+		100%{
+			transform:scale(1);
+		}
+	}
+	.categoryBox{
+		width: 100%;
+		height: 100%;
+		.categoryArea{
+			width: 100%;
+			height: 100%;
+			>ul{
+				position: relative;
+				z-index: 2;
+				clear: both;
+				overflow: hidden;
+				padding: 10px 15px;
+				background: #2f3b49;
+				color: #fff;
+				animation: myMove 0.3s ease-out;
+				li{
+					float: left;
+					width: 25%;
+					margin-bottom: 8px;
+					text-align: center;
+					img{
+						width: 50px;
+						height: 50px;
+						border-radius: 50%;
+
+					}
+					p{
+						line-height: 2;
+						font-size: 12px;
+					}
+				}
+			}
+		}
+		.uploadArea{
+			width: 100%;
+			height: 100%;
+			position: relative;
+			>.uploadcontent{
+				position: absolute;
+				left: 50%;
+				top:40%;
+				width: 80%;
+				background: #2f3b49;
+				clear:both;
+				color: #fff;
+				padding: 10px 15px;
+				overflow: hidden;
+				z-index: 2;
+				transform:translate(-50%,-50%);
+				p{
+					line-height: 2;
+					font-size: 12px;
+					margin-bottom: 15px;
+				}
+				li{
+					width: 33%;
+					float: left;
+					text-align: center;
+					img{
+						width: 50px;
+						height: 50px;
+						animation: imgscale 0.4s ease-out;
+						border-radius: 50%;
+					}
+				}
+			}
+		}
+		.mask{
+			position: absolute;
+			left:0;
+			top:0;
+			height: 100%;
+			width: 100%;
+			z-index: 1;
+			opacity: 0.4;
+			background: rgb(0,0,0);
+		}
+	}
+</style>
